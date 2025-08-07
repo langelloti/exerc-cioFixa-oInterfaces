9c4fc7bc407fee9c803bffc60b4db80ba20cae93
Lucas <lucasangelloti@gmail.com>
2025-08-07 09:25:22 -0300

    Terceiro Commit
    ajustando e saindo pra trabalhar, depois continuar

--- main/src/application/Program.java
+++ main/src/application/Program.java
@@ -1,8 +1,30 @@
-package Program;
+package application;
 
 
-public class Main {
+import exercise.model.entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Enter the contract data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Contract total value: R$ ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+        System.out.println(contract);
     }
 }
--- main/src/exercise/model/entities/Installment.java
+++ main/src/exercise/model/entities/Installment.java
@@ -1,5 +1,33 @@
 package exercise.model.entities;
 
-public class Installment {
-    
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Installment extends Contract {
+    private LocalDate dueDate;
+    private Double amount;
+
+    public Installment() {
+    }
+
+    public Installment(LocalDate dueDate, Double amount) {
+        this.dueDate = dueDate;
+        this.amount = amount;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
